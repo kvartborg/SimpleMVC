40a9b6b44f07e93d7199849da6489b5b423fae4b
Frederik Kvartborg Albertsen <fka@skemax.dk>
2015-02-05 10:31:58 +0100

    Changed the index file to redirect into public folder on all domains

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <html>
 <head>
-  <meta charset="utf-8"> 
-  <meta http-equiv="cache-control" content="max-age=0" />
-  <meta http-equiv="cache-control" content="no-cache" />
-  <meta http-equiv="expires" content="0" />
-  <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
-  <meta http-equiv="pragma" content="no-cache" />
-  <meta http-equiv="refresh" content="0; url=https://kvartborg.net/hello" />
-  <base href="https://kvartborg.net/public/" />
-  <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+  <meta http-equiv="refresh" content="0; url=http://<?php echo $_SERVER['SERVER_NAME']; ?>/hello" />
+  <base href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/public/" />
 </head>
 
 <body>

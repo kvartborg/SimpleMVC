c1b1590ccd9e9129542f965785ed50b8981fb6bb
Frederik Kvartborg Albertsen <kvartborg@Frederiks-iMac.local>
2015-02-11 23:28:33 +0100

    Removed the composer and vendor files

--- app/init.php
+++ app/init.php
@@ -3,9 +3,6 @@
 //	Initialize bootstrap
 // ------------------------------------
 
-// composer autoloader
-require_once "../vendor/autoload.php";
-
 // aplication files
 require_once "core/App.php";
 require_once "core/BaseController.php";
--- app/models/User.php
+++ app/models/User.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Database\Eloquent\Model as Eloquent;
-
-class User extends Eloquent { 
+class User { 
 
 }

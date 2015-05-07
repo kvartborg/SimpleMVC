130572adb887bf4ca6cc268012257ad302e0c9db
Frederik Kvartborg <fka@skemax.dk>
2015-05-07 11:46:55 +0200

    Added som more functionality to the Time/Date class

--- app/core/Time.php
+++ app/core/Time.php
@@ -13,6 +13,11 @@ class Time {
   }
 
 
+  protected function daysInMonth($month, $year){ 
+    return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31); 
+  } 
+
+
   protected function leapYear($year){
     return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year %400) == 0)));
   }
@@ -27,6 +32,7 @@ class Time {
 
   public static function parse($time){
     $time = str_replace('/', '-', $time);
+    $time = str_replace('.', '-', $time);
     $obj = new Time;
 
     if(!is_numeric($time)){
@@ -63,92 +69,131 @@ class Time {
   }
 
 
-  public function addSeconds($time){
-    $this->time = $this->time + $time;
-    return $this;
-  }
-
-
-  public function addMinutes($time){
-    $time = $time * 60;
-    $this->time = $this->time + $time;
-    return $this;
-  }
-
-
-  public function addHours($time){
-    $time = $time * 60 * 60;
-    $this->time = $this->time + $time;
-    return $this;
-  }
-
-
-  public function addDays($time){
-    $time = $time * 60 * 60 * 24;
-    $this->time = $this->time + $time;
-    return $this;
-  }
-
-
-  public function addYears($time){
-    $n = $time;
-    $year = intval(date('Y', $this->time));
-    $time = 24 * 60 * 60;
-    for ($i = 0; $i < $n; $i++) { 
-      if($this->leapYear($year + $i + 1))
-        $this->time += $time * (365 + 1);
-      else
-        $this->time += $time * 365;
-    } 
-
-    return $this;
-  }
-
-
-  public function subSeconds($time){
-    $this->time = $this->time - $time;
-    return $this;
-  }
-
-
-  public function subMinutes($time){
-    $time = $time * 60;
-    $this->time = $this->time - $time;
-    return $this;
-  }
-
-
-  public function subHours($time){
-    $time = $time * 60 * 60;
-    $this->time = $this->time - $time;
-    return $this;
-  }
-
-
-  public function subDays($time){
-    $time = $time * 60 * 60 * 24;
-    $this->time = $this->time - $time;
-    return $this;
-  }
-
-
-  public function subYears($time){
-    $n = $time;
-    $year = intval(date('Y', $this->time));
-    $time = 24 * 60 * 60;
-    for ($i = 0; $i < $n; $i++) { 
-      if($this->leapYear($year + $i - 1))
-        $this->time -= $time * (365 + 1);
-      else
-        $this->time -= $time * 365;
-    } 
-
-    return $this;
-  }
-
-
   public function gm(){
     $this->return = gmdate($this->format, $this->time);
+    return $this;
+  }
+
+
+  public function addSeconds($sec){
+    $this->time = $this->time + $sec;
+    return $this;
+  }
+
+
+  public function addMinutes($min){
+    $min = $min * 60;
+    $this->time = $this->time + $min;
+    return $this;
+  }
+
+
+  public function addHours($hours){
+    $hours = $hours * 60 * 60;
+    $this->time = $this->time + $hours;
+    return $this;
+  }
+
+
+  public function addDays($days){
+    $days = $days * 60 * 60 * 24;
+    $this->time = $this->time + $days;
+    return $this;
+  }
+
+
+  public function addWeeks($weeks){
+    $weeks = $weeks * 7 * 24 * 60 * 60;
+    $this->time = $this->time + $weeks;
+    return $this;
+  }
+
+
+  public function addMonths($months){
+    $n = $months;
+
+    for ($i = 0; $i < $n; $i++) { 
+      $month = intval(date('m', $this->time));
+      $year = intval(date('Y', $this->time));
+      $this->time = $this->time + $this->daysInMonth($month, $year) * 24 * 60 * 60;
+    }
+
+    return $this;
+  }
+
+  public function addYears($years){
+    $n = $years;
+    $year = intval(date('Y', $this->time));
+    $years = 24 * 60 * 60;
+    for ($i = 0; $i < $n; $i++) { 
+      if($this->leapYear($year + $i + 1))
+        $this->time += $years * (365 + 1);
+      else
+        $this->time += $years * 365;
+    } 
+
+    return $this;
+  }
+
+
+  public function subSeconds($sec){
+    $this->time = $this->time - $sec;
+    return $this;
+  }
+
+
+  public function subMinutes($min){
+    $min = $min * 60;
+    $this->time = $this->time - $min;
+    return $this;
+  }
+
+
+  public function subHours($hours){
+    $hours = $hours * 60 * 60;
+    $this->time = $this->time - $hours;
+    return $this;
+  }
+
+
+  public function subDays($days){
+    $days = $days * 60 * 60 * 24;
+    $this->time = $this->time - $days;
+    return $this;
+  }
+
+
+  public function subWeeks($weeks){
+    $weeks = $weeks * 7 * 24 * 60 * 60;
+    $this->time = $this->time - $weeks;
+    return $this;
+  }
+
+
+  public function subMonths($months){
+    $n = $months;
+
+    for ($i = 0; $i < $n; $i++) { 
+      $month = intval(date('m', $this->time));
+      $year = intval(date('Y', $this->time));
+      $this->time = $this->time - $this->daysInMonth($month, $year) * 24 * 60 * 60;
+    }
+
+    return $this;
+  }
+
+
+  public function subYears($years){
+    $n = $years;
+    $year = intval(date('Y', $this->time));
+    $years = 24 * 60 * 60;
+    for ($i = 0; $i < $n; $i++) { 
+      if($this->leapYear($year + $i - 1))
+        $this->time -= $years * (365 + 1);
+      else
+        $this->time -= $years * 365;
+    } 
+
     return $this;
   }
 

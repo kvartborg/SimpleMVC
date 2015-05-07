6c0fcbeb4406e49ec01d5208772dee163266658f
Frederik Kvartborg <fka@skemax.dk>
2015-05-07 12:02:16 +0200

    This closes #2

--- app/controllers/HelloController.php
+++ app/controllers/HelloController.php
@@ -14,9 +14,6 @@ class HelloController extends BaseController {
 
   public function test(){
     $time = Time::now();
-    echo $time; 
-    Error::set('Couldn\'t find the route', __FILE__, __LINE__);
-    Error::set('This is very cool!!', __FILE__, __LINE__);
-    Error::set('This just another error', __FILE__, __LINE__);
+    echo $time;
   }
 }

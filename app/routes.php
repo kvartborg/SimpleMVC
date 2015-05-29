12ef2335414dbebf06f4e86a801eecfb9baa10c0
Frederik Kvartborg <fka@skemax.dk>
2015-05-29 18:18:43 +0200

    Fixed issue with routes regarding to the controller methods

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,9 @@
 <?php
 
 Route::set('/hello', 'HelloController');
+
+Route::get('/', function(){
+  return View::make('index', ['users' => DB::table('users')->get()]);
+});
+
+Route::set('/test', 'TestController');

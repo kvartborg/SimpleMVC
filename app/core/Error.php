8e8e651806a73f8f3148e92d600f370f2efb6c1f
Frederik Kvartborg <fka@skemax.dk>
2015-09-10 15:10:13 +0200

    Added layout to the error screen

--- app/core/Error.php
+++ app/core/Error.php
@@ -37,19 +37,68 @@ function fatalErrorHandler() {
     $html = '<html>';
 
     $html .= '<head>'
+              .'<title>Ups, looks like you broke it.</title>'
               .'<style>'
-                .'body { overflow: hidden; font-family: helvetica, verdana }'
-                .'#view { background: #f8f8f8; position: absolute; top: 0; left: 0; width: 100%; height: 100%; overflow: auto; }'
-                .'#errors { width: 80%; margin-left: 10%;  }'
+                .'body { 
+                  overflow: hidden; 
+                  font-family: gesta, helvetica, verdana;
+                  color: #555;
+                }'
+                
+                .'#view { 
+                  background: #fff; 
+                  position: absolute; 
+                  top: 0;  left: 0; 
+                  width: 100%; 
+                  height: 100%; 
+                  overflow: auto; 
+                }'
+
+                .'h1 {
+                  color: #373F47;
+                  font-family: facit;
+                  margin-top: 100px;
+                  margin-bottom: 80px;
+                }'
+
+                .'h1:before {
+                  content: "#";
+                  display:inline-block;
+                  margin-right: 20px;
+                  margin-left: -40px;
+                  font-weight: 300;
+                  color: #8AA39B;
+                }'
+
+                .'.item {
+                  padding-bottom: 25px;
+                  padding-top: 10px;
+                  box-shadow: inset 0px -1px rgba(0,0,0,0.06);
+                }'
+
+                .'.item:last-child {
+                  box-shadow: inset 0px -1px rgba(0,0,0,0.0);
+                }'
+
+                .'h3 {
+                  color: #8AA39B;
+                  margin-bottom: 5px;
+                }'
+
+                .'#errors {
+                  margin-right: 15%;
+                  margin-left: 15%; 
+                }'
               .'</style>'
-            .'</head><body><div id="view"><div id="errors">';
+            .'</head><body><div id="view"><div id="errors">'
+            .'<h1 class="heading">Ups, looks like something failed...</h1>';
 
     for($i = 0; $i < count($errors); $i++){
       $error = $errors[$i];
-      $html .= '<p>'
+      $html .= '<div class="item">'
                 .'<h3>'.$error['str'].', on line '.$error['line'].'</h3>'
                 .'<small>'.$error['file'].'</small>'
-             .'</p><hr>';
+             .'</div>';
     }
 
     $html .= '</div></div></body></html>';

c631c27e7d2a8afc0ba614d967ee2d0ea83eae80
Frederik Kvartborg <fka@skemax.dk>
2015-11-30 22:51:51 +0100

    Added debug argument and data object

--- app/modules/Event.php
+++ app/modules/Event.php
@@ -3,27 +3,40 @@
 class Event {
 
   protected $queue = [];
+  protected $args  = [];
+  protected $data  = [];
 
-  public function __construct(){
-  
+  public function __construct($args = []){
+    $this->args = array_values($args);
+
+    $this->data = new stdClass();
+    for($i = 0; $i < count($this->args); $i++){
+      if(strpos($this->args[$i], '=')){
+        $item = explode('=', $this->args[$i]);
+        $this->data->$item[0] = $item[1];
+      }
+    }
   }
 
 
   public static function run($event, $data = []){
     $e = new Event;
-    if(is_array($event)){
-      $command = '';
-      for($i = 0; $i < count($event); $i++){
-        $command .= 'php specla '.$event.' > /dev/null 2>/dev/null; ';
-      }
-      $command .= '&';
-    } else {
-      $command = 'php specla '.$event.' > /dev/null 2>/dev/null &';
-    }
+
+    $command = 'php specla '.$event.' '.join(' ', $data).' > /dev/null 2>/dev/null &';
     
     exec($command);
   }
 
+
+  public function debug($str){
+    if(in_array('debug', $this->args)){
+      if(is_string($str)){
+        echo $str."\n";
+      } else {
+        echo json_encode($str, JSON_PRETTY_PRINT)."\n";
+      }
+    }
+  }
 
   public function info($str){
     echo "\033[32m".$str."\033[0m\n";

dc3023495b8121380b0143a31586b9ef3b40401f
Frederik Kvartborg <fka@skemax.dk>
2016-02-02 23:12:41 +0100

    Removed old config and added example

---
+++ app/config.example.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+  'debug' => true,
+
+  'ssl' => true,
+  'composer' => false,
+  'key' => '1234',
+
+
+  'database' => [
+    'driver'    => '',
+    'host'      => '',
+    'database'  => '',
+    'username'  => '',
+    'password'  => '',
+    'prefix'    => '',
+  ],
+
+  'language'    => 'en',
+  'timezone'    => 'Europe/Copenhagen',
+  'date_format' => 'Y-m-d H:i:s',
+
+
+  'folders' => ['modules', 'models', 'helpers'],
+
+];

315412b367a900d9ae0375e5a334b09f2aff2d2a
Frederik Kvartborg <fka@skemax.dk>
2016-02-24 22:29:38 +0100

    Added Router and Route

---
+++ app/modules/router/Route.php
@@ -0,0 +1,103 @@
+<?php
+
+class Route {
+
+  /**
+   * $path the routes path
+   * @var string
+   */
+  public $path;
+
+  /**
+   * $options contains all options for a route
+   * @var array
+   */
+  public $options = [];
+
+
+  /**
+   * Register new route
+   * @param string  $method     The accepted HTTP method
+   * @param  string          $path         The path that should be defined
+   * @param  bool|closure    $middleware   Should be true to accept the route (Optional)
+   * @param  closure|null    $callback     What the route does
+   * @return Router          Instance of the router
+   */
+  public function __construct($method, $path, $middleware, $callback = null){ 
+
+    if(is_null($callback)){
+      $callback = $middleware;
+      $middleware = true;
+    }
+
+    if($path[0] !== '/')
+      $path = '/'.$path;
+
+    if(strlen($path) > 1)
+      $path = rtrim($path, '/');
+
+    $this->path = $path; 
+    $this->options['method'] = $method;
+    $this->options['middleware'] = $middleware;
+    $this->options['callback'] = $callback;
+
+    return $this->register();
+  }
+
+
+  /**
+   * Defined routes with HTTP methods
+   * @param  string          $path         The path that should be defined
+   * @param  bool|closure    $middleware   Should be true to accept the route (Optional)
+   * @param  closure|null    $callback     What the route does
+   * @return Router          Instance of the router
+   */
+  public static function all($path, $middleware, $callback = null){ return new Route('ALL', $path, $middleware, $callback); }
+  public static function get($path, $middleware, $callback = null){ return new Route('GET', $path, $middleware, $callback); }
+  public static function post($path, $middleware, $callback = null){return new Route('POST', $path, $middleware, $callback); }
+  public static function put($path, $middleware, $callback = null){ return new Route('PUT', $path, $middleware, $callback); }
+  public static function delete($path, $middleware, $callback = null){ return new Route('DELETE', $path, $middleware, $callback); }
+
+
+  /**
+   * Do something when nothing matched the REQUEST_URI
+   * @param  closure $callback what to do
+   * @return null
+   */
+  public static function missing($callback, $code = 404){ 
+    http_response_code($code);
+    Router::$missing = $callback; 
+  }
+  
+
+  /**
+   * Catch requests if no path was found
+   * @param  closure $callback what to do
+   * @return null
+   */
+  public static function catchAll($callback, $code = 200){
+    http_response_code($code);
+    Router::$missing = $callback;
+  }
+
+
+  /**
+   * Add a name to the route options 
+   * @param  string $str Name for the route
+   * @return Route      
+   */
+  public function name($str){
+    $this->options['name'] = $str;
+    return $this->register();
+  }
+
+
+  /**
+   * Register all changes to the route object and add it to the Router::$paths
+   * @return Route
+   */
+  public function register(){
+    Router::$paths[$this->path] = $this->options;
+    return $this;
+  }
+}
---
+++ app/modules/router/Router.php
@@ -0,0 +1,194 @@
+<?php
+
+class Router {
+
+  /**
+   * $paths is a dictionary with all routes and its options
+   * @var array
+   */
+  public static $paths = [];
+
+  /**
+   * $uri the current URI of the app
+   * @var string
+   */
+  protected $uri;
+
+  /**
+   * $pathsWithoutVars paths without variables
+   * @var array
+   */
+  protected static $pathsWithoutVars = [];
+
+  /**
+   * $pathsWithVars paths which contains variables
+   * @var array
+   */
+  protected static $pathsWithVars = [];
+
+  /**
+   * $vars contains variables for routes
+   * @var array
+   */
+  protected $vars = [];
+
+  /**
+   * Contains the catch all closure 
+   * @var null
+   */
+  public static $missing = null;
+
+
+  /**
+   * Register router
+   * Get the app URI
+   * Short paths
+   * Find matching path
+   */
+  public function __construct(){
+    $this->uri = rtrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
+
+    if($this->uri === '')
+      $this->uri = '/';
+
+    $this->shortPaths();
+    $this->find();
+  }
+
+  /**
+   * Short registered paths into to arrays (paths without vars, paths with vars)
+   * @return null
+   */
+  protected function shortPaths(){
+    foreach(static::$paths as $path => $options) {
+      if(strpos($path, ':')){
+        static::$pathsWithVars[$path] = $options;
+      } else {
+        static::$pathsWithoutVars[$path] = $options;
+      }
+    }
+  }
+
+
+  /**
+   * Checks paths against REQUEST_URI to find a match
+   * @return null
+   */
+  protected function find(){
+    foreach(static::$pathsWithoutVars as $path => $options){
+      if($this->uri === $path 
+        && $options['middleware'] === true
+        && ($options['method'] === $_SERVER['REQUEST_METHOD'] || $options['method'] === 'ALL')
+      ){
+        return $this->trigger($path, $options);
+      }
+    }
+
+    $this->checkForVars();
+  }
+
+
+  /**
+   * Iterates over paths with vars
+   * @return null 
+   */
+  protected function checkForVars(){
+    foreach(static::$pathsWithVars as $path => $options){
+      if($this->uri === $this->constructURI($path) 
+        && $options['middleware'] === true
+        && ($options['method'] === $_SERVER['REQUEST_METHOD'] || $options['method'] === 'ALL')
+      ){
+        return $this->trigger($path, $options);
+      }
+    }
+
+    $this->notFound();
+  }
+
+
+  /**
+   * Constructs a real uri with variables from the REQUEST_URI
+   * @param  string $path Create real URI from path with variables
+   * @return string       Constructed path
+   */
+  protected function constructURI($path){
+    $this->vars = [];
+    $uri = explode('/', ltrim($this->uri, '/'));
+    $pathURI = explode('/', ltrim($path, '/'));
+    $constructURI = [];
+
+    for($i = 0; $i < count($pathURI); $i++){
+      if(strpos($pathURI[$i], ':') !== false && strpos($pathURI[$i], '?')){
+        // optional
+        if(isset($uri[$i]))
+          $this->vars[] = $uri[$i];
+
+        $constructURI[$i] = isset($uri[$i]) ? $uri[$i] : '';
+      } elseif(strpos($pathURI[$i], ':') !== false) {
+        // required
+        if(isset($uri[$i])){
+          $this->vars[] = $uri[$i];
+          $constructURI[$i] = $uri[$i];
+        } else {
+          $constructURI[$i] = $pathURI[$i];
+        }
+      } else {
+        $constructURI[$i] = $pathURI[$i];
+      }
+    }
+
+    return rtrim('/'.join($constructURI, '/'), '/');
+  }
+
+  /**
+   * Trigger the route callback
+   * @param  string $path    The matched path
+   * @param  array  $options Contains middleware, name, closure and more...
+   * @return null
+   */
+  protected function trigger($path, $options){
+    if(is_callable($options['callback'])){
+      call_user_func_array($options['callback'], $this->vars);
+    } else {
+      $this->controller($options['callback']);
+    }
+  }
+
+  /**
+   * Parse the controller callback string
+   * @param  string $callback Controller string
+   * @return null
+   */
+  protected function controller($callback){
+    $class = explode('@', $callback)[0];
+    $method = explode('@', $callback)[1];
+    $root = $_SERVER['DOCUMENT_ROOT'].str_replace(['/index.php', '/public/index.php'], '', $_SERVER['SCRIPT_NAME']);
+
+    if(file_exists($root.'/app/controllers/'.$class.'.php')){
+      require_once $root.'/app/controllers/'.$class.'.php';
+    } else {
+      throw new Exception('Missing the '.$class);
+    }
+
+    $controller = new $class;
+    if(method_exists($controller, $method)){
+      call_user_func_array([new $class, $method], $this->vars);
+    } else {
+      throw new Exception($class.' is missing the '.$method.' method');
+    }
+  }
+
+
+  /**
+   * If no path match the REQUEST_URI, then fire the not found
+   * @return null
+   */
+  public function notFound(){
+    if(is_null(static::$missing)){
+      http_response_code(404);
+      throw new Exception("Couldn't find any matching routes");
+    } else {
+      call_user_func(static::$missing);
+    }
+  }
+}
